3c9e76a21adb168e4a21c91b7861ba55f9014d4b
CodeInferno03 <90342800+CodeInferno03@users.noreply.github.com>
2023-02-15 21:38:35 +0000

    starting other program

---
+++ Python Monster Rune storage/storer.py
@@ -0,0 +1,10 @@
+# program to store the monsters who need rune changes
+import pandas as pd
+
+
+# text file to store in
+textfile = "save_text.json"
+
+# the dict that prints to the json file
+json_dict = dict()
+
